6b41e0f79a773fd6de29067f3f7fb2453ac4c582
goedgecloud <goedgecloud@gmail.com>
2021-07-21 22:11:59 +0800

    编译脚本增加arm64

--- build/build.sh
+++ build/build.sh
@@ -64,7 +64,7 @@ function build() {
 
 	# building installer
 	echo "building installer ..."
-	architects=("amd64" "386")
+	architects=("amd64" "386" "arm64")
 	for arch in "${architects[@]}"; do
 		# TODO support arm, mips ...
 		env GOOS=linux GOARCH=${arch} go build --ldflags="-s -w" -o $ROOT/installers/edge-installer-helper-linux-${arch} $ROOT/../cmd/installer-helper/main.go
