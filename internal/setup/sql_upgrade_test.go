c02765c27a615506b0387c3674c86efa418f7977
goedgecloud <goedgecloud@gmail.com>
2021-08-06 14:47:42 +0800

    修改一处测试

--- internal/setup/sql_upgrade_test.go
+++ internal/setup/sql_upgrade_test.go
@@ -6,7 +6,11 @@ import (
 )
 
 func TestUpgradeSQLData(t *testing.T) {
-	db, err := dbs.Default()
+	db, err := dbs.NewInstanceFromConfig(&dbs.DBConfig{
+		Driver: "mysql",
+		Dsn:    "root:123456@tcp(127.0.0.1:3306)/db_edge_new?charset=utf8mb4&timeout=30s",
+		Prefix: "edge",
+	})
 	if err != nil {
 		t.Fatal(err)
 	}
