c1256ff47d9df2c89d5b7c3e125d79f5be53aeb2
goedgecloud <goedgecloud@gmail.com>
2021-11-24 19:57:15 +0800

    修改用户节点版本为0.2.0

--- internal/const/const.go
+++ internal/const/const.go
@@ -19,7 +19,7 @@ const (
 	// 其他节点版本号，用来检测是否有需要升级的节点
 
 	NodeVersion          = "0.3.6"
-	UserNodeVersion      = "0.0.10"
+	UserNodeVersion      = "0.2.0"
 	AuthorityNodeVersion = "0.0.2"
 	MonitorNodeVersion   = "0.0.3"
 	DNSNodeVersion       = "0.2.0"
