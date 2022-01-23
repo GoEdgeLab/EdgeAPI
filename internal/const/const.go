8260457ade2c49e8d057437bfcd5a6d560c4a5e0
goedgecloud <goedgecloud@gmail.com>
2022-01-23 19:15:45 +0800

    用户版本改为0.3.1

--- internal/const/const.go
+++ internal/const/const.go
@@ -19,7 +19,7 @@ const (
 	// 其他节点版本号，用来检测是否有需要升级的节点
 
 	NodeVersion          = "0.4.1"
-	UserNodeVersion      = "0.3.0"
+	UserNodeVersion      = "0.3.1"
 	AuthorityNodeVersion = "0.0.2"
 	MonitorNodeVersion   = "0.0.3"
 	DNSNodeVersion       = "0.2.1"
