95cfd2ff6c2687b7a12759ba58bf117ddb6ca0cb
goedgecloud <goedgecloud@gmail.com>
2022-02-21 18:31:47 +0800

    修改版本为v0.4.3

--- internal/const/const.go
+++ internal/const/const.go
@@ -1,7 +1,7 @@
 package teaconst
 
 const (
-	Version = "0.4.2"
+	Version = "0.4.3"
 
 	ProductName   = "Edge API"
 	ProcessName   = "edge-api"
@@ -18,7 +18,7 @@ const (
 
 	// 其他节点版本号，用来检测是否有需要升级的节点
 
-	NodeVersion          = "0.4.2"
+	NodeVersion          = "0.4.3"
 	UserNodeVersion      = "0.3.1"
 	AuthorityNodeVersion = "0.0.2"
 	MonitorNodeVersion   = "0.0.3"
