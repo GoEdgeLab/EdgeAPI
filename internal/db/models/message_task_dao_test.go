c6c7fa279739e8d740a5a792cd3433312641b9ae
goedgecloud <goedgecloud@gmail.com>
2022-12-06 14:53:38 +0800

    增加测试用例

--- internal/db/models/message_task_dao_test.go
+++ internal/db/models/message_task_dao_test.go
@@ -1,6 +1,7 @@
-package models
+package models_test
 
 import (
+	"github.com/TeaOSLab/EdgeAPI/internal/db/models"
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/iwind/TeaGo/bootstrap"
 	"github.com/iwind/TeaGo/dbs"
@@ -11,12 +12,21 @@ func TestMessageTaskDAO_FindSendingMessageTasks(t *testing.T) {
 	dbs.NotifyReady()
 
 	var tx *dbs.Tx
-	tasks, err := NewMessageTaskDAO().FindSendingMessageTasks(tx, 100)
+	tasks, err := models.NewMessageTaskDAO().FindSendingMessageTasks(tx, 100)
 	if err != nil {
 		t.Fatal(err)
 	}
 	t.Log(len(tasks), "tasks")
 	for _, task := range tasks {
 		t.Log("task:", task.Id, "recipient:", task.RecipientId)
+	}
+}
+
+func TestMessageTaskDAO_CleanExpiredMessageTasks(t *testing.T) {
+	var dao = models.NewMessageTaskDAO()
+	var tx *dbs.Tx
+	err := dao.CleanExpiredMessageTasks(tx, 30)
+	if err != nil {
+		t.Fatal(err)
 	}
 }
