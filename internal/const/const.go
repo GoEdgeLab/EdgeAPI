6da7ca6f71c73832a56beebb8e63300d554cc6b5
goedgecloud <goedgecloud@gmail.com>
2023-04-10 09:18:12 +0800

    版本号修改为1.0.1

--- internal/const/const.go
+++ internal/const/const.go
@@ -1,7 +1,7 @@
 package teaconst
 
 const (
-	Version = "1.0.0"
+	Version = "1.0.1"
 
 	ProductName   = "Edge API"
 	ProcessName   = "edge-api"
@@ -18,7 +18,7 @@ const (
 
 	// 其他节点版本号，用来检测是否有需要升级的节点
 
-	NodeVersion = "1.0.0"
+	NodeVersion = "1.0.1"
 
 	// SQLVersion SQL版本号
 	SQLVersion = "11"
