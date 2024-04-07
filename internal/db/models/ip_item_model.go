c7fb18094a510743863b5d3e92dbab00c10613a5
goedgecloud <goedgecloud@gmail.com>
2024-04-07 11:26:56 +0800

    标记edgeIPItems中两个字段为弃用

--- internal/db/models/ip_item_model.go
+++ internal/db/models/ip_item_model.go
@@ -1,5 +1,33 @@
 package models
 
+import "github.com/iwind/TeaGo/dbs"
+
+const (
+	IPItemField_Id                            dbs.FieldName = "id"                            // ID
+	IPItemField_ListId                        dbs.FieldName = "listId"                        // 所属名单ID
+	IPItemField_Type                          dbs.FieldName = "type"                          // 类型
+	IPItemField_IpFrom                        dbs.FieldName = "ipFrom"                        // 开始IP
+	IPItemField_IpTo                          dbs.FieldName = "ipTo"                          // 结束IP
+	IPItemField_IpFromLong                    dbs.FieldName = "ipFromLong"                    // 开始IP整型（弃用）
+	IPItemField_IpToLong                      dbs.FieldName = "ipToLong"                      // 结束IP整型（弃用）
+	IPItemField_Version                       dbs.FieldName = "version"                       // 版本
+	IPItemField_CreatedAt                     dbs.FieldName = "createdAt"                     // 创建时间
+	IPItemField_UpdatedAt                     dbs.FieldName = "updatedAt"                     // 修改时间
+	IPItemField_Reason                        dbs.FieldName = "reason"                        // 加入说明
+	IPItemField_EventLevel                    dbs.FieldName = "eventLevel"                    // 事件级别
+	IPItemField_State                         dbs.FieldName = "state"                         // 状态
+	IPItemField_ExpiredAt                     dbs.FieldName = "expiredAt"                     // 过期时间
+	IPItemField_ServerId                      dbs.FieldName = "serverId"                      // 有效范围服务ID
+	IPItemField_NodeId                        dbs.FieldName = "nodeId"                        // 有效范围节点ID
+	IPItemField_SourceNodeId                  dbs.FieldName = "sourceNodeId"                  // 来源节点ID
+	IPItemField_SourceServerId                dbs.FieldName = "sourceServerId"                // 来源服务ID
+	IPItemField_SourceHTTPFirewallPolicyId    dbs.FieldName = "sourceHTTPFirewallPolicyId"    // 来源策略ID
+	IPItemField_SourceHTTPFirewallRuleGroupId dbs.FieldName = "sourceHTTPFirewallRuleGroupId" // 来源规则集分组ID
+	IPItemField_SourceHTTPFirewallRuleSetId   dbs.FieldName = "sourceHTTPFirewallRuleSetId"   // 来源规则集ID
+	IPItemField_SourceUserId                  dbs.FieldName = "sourceUserId"                  // 用户ID
+	IPItemField_IsRead                        dbs.FieldName = "isRead"                        // 是否已读
+)
+
 // IPItem IP
 type IPItem struct {
 	Id                            uint64 `field:"id"`                            // ID
@@ -7,8 +35,8 @@ type IPItem struct {
 	Type                          string `field:"type"`                          // 类型
 	IpFrom                        string `field:"ipFrom"`                        // 开始IP
 	IpTo                          string `field:"ipTo"`                          // 结束IP
-	IpFromLong                    uint64 `field:"ipFromLong"`                    // 开始IP整型
-	IpToLong                      uint64 `field:"ipToLong"`                      // 结束IP整型
+	IpFromLong                    uint64 `field:"ipFromLong"`                    // 开始IP整型（弃用）
+	IpToLong                      uint64 `field:"ipToLong"`                      // 结束IP整型（弃用）
 	Version                       uint64 `field:"version"`                       // 版本
 	CreatedAt                     uint64 `field:"createdAt"`                     // 创建时间
 	UpdatedAt                     uint64 `field:"updatedAt"`                     // 修改时间
@@ -33,8 +61,8 @@ type IPItemOperator struct {
 	Type                          any // 类型
 	IpFrom                        any // 开始IP
 	IpTo                          any // 结束IP
-	IpFromLong                    any // 开始IP整型
-	IpToLong                      any // 结束IP整型
+	IpFromLong                    any // 开始IP整型（弃用）
+	IpToLong                      any // 结束IP整型（弃用）
 	Version                       any // 版本
 	CreatedAt                     any // 创建时间
 	UpdatedAt                     any // 修改时间
