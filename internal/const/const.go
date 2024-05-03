68cca05313e97b51dadcff334281fb924233ffbe
goedgecloud <goedgecloud@gmail.com>
2024-05-03 11:21:54 +0800

    版本号修改为1.3.8.2

--- internal/const/const.go
+++ internal/const/const.go
@@ -1,7 +1,7 @@
 package teaconst
 
 const (
-	Version = "1.3.8"
+	Version = "1.3.8.2"
 
 	ProductName   = "Edge API"
 	ProcessName   = "edge-api"
@@ -11,14 +11,11 @@ const (
 
 	Role = "api"
 
-	EncryptKey    = "8f983f4d69b83aaa0d74b21a212f6967"
 	EncryptMethod = "aes-256-cfb"
-
-	ErrServer = "服务器出了点小问题，请稍后重试"
 
 	SystemdServiceName = "edge-api"
 
 	// 其他节点版本号，用来检测是否有需要升级的节点
 
-	NodeVersion = "1.3.8"
+	NodeVersion = "1.3.8.2"
 )
